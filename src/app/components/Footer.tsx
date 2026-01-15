3a8cf814f5cc4e3fcb783ed9a521cb0399c1063e
Michał Kotfica <kotvitz17@gmail.com>
2026-01-15 01:18:28 +0100

    feat: add footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,15 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
+
   return (
-    <></>
+    <footer className="border-t bg-white">
+      <div className="container-page py-6">
+        <div className="text-center text-sm text-gray-500">
+          <div>
+            © {year} Rafał Kubowicz. Wszelkie prawa zastrzeżone.
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
